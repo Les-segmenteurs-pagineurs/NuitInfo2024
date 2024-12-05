f5ed8a3cc2d16c4c2aa44478663e2dc106e716ab
= <=>
2024-12-05 22:31:12 +0100

    feat(homegamepage): add home screen logic & styles

---
+++ components/HumanDominantDisplay.vue
@@ -0,0 +1,32 @@
+<template>
+  
+  <div class="main">
+    <div class="left-side">
+      
+      <div class="corner-pic">
+        <slot />
+      </div>
+      <div>
+        <ul>
+          <li>stat</li>
+          <li>stat</li>
+          <li>stat</li>
+        </ul>
+      </div>
+    </div>
+    <div class="w-100 h-100 right-side">
+      content
+    </div>
+  </div>
+  
+
+
+
+</template>
+
+
+<script setup lang="ts">
+import '~/assets/css/game.scss'
+import '~/assets/css/bootstrapWish.scss'
+
+</script>
