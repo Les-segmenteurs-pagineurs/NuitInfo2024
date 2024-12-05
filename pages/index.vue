f945233ae013448e2f4d96582fb2d76cb77efc98
= <=>
2024-12-05 20:03:16 +0100

    <feat>(homepage) : circle radiant and titel

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,22 @@ const toast = useToast()
 </script>
 
 <template>
-  <h1>home page</h1>
+  <div class="circle-div bg-gradient-to-r from-slate-900 to-transparent">
+    <h1 class="title">Un site par les Segmenteurs Pagineurs</h1>
+  </div>
 </template>
 
 <style scoped>
-
+  .circle-div{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    width: 50rem;
+    height: 50rem;
+  }
+  .title{
+    font-size: xx-large;
+    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+  }
 </style>
