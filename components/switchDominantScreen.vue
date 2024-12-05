56be6d542ec7c2116fd9bfd3bbbffb89f836cb43
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2024-12-05 20:02:57 +0100

    feat(switchDominantScrenn): add switch dominant screen between Ocean & Human

---
+++ components/switchDominantScreen.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { useRoute } from "#app";
+
+const page = useRoute();
+const actualPage = page.name;
+let nextPage = "";
+
+if (actualPage === "humanDominant") {
+  nextPage = "oceanDominant";
+} else {
+  nextPage = "humanDominant";
+}
+</script>
+
+<template>
+  <button>
+    <NuxtLink :to="`/${nextPage}`">
+      <Icon name="i-heroicons-arrows-right-left" style="color: white" />
+    </NuxtLink>
+  </button>
+</template>
+
+<style lang="css" scoped></style>
---
+++ pages/humanDominant.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import SwitchDominantScreen from "~/components/switchDominantScreen.vue";
+</script>
+
+<template>
+  <div class="minor">
+    <h1>Minor Ocean</h1>
+    <p>Minor Ocean Page</p>
+  </div>
+  <div class="dominant">
+    <h1>Human Dominant</h1>
+    <p>Human Dominant Page</p>
+  </div>
+  <div><SwitchDominantScreen /></div>
+</template>
+
+<style>
+.minor {
+  background-color: red;
+}
+
+.dominant {
+  background-color: blue;
+}
+</style>
