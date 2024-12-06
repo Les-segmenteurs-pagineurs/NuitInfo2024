6575d522a1bab2bbed42fd5c929c452cf537618b
Sylvain <sylvainschaefer@gmail.com>
2024-12-06 03:47:12 +0100

    fix: make responsive

--- pages/pokedex/[name].vue
+++ pages/pokedex/[name].vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="p-4 w-3/5 self-center min-h-screen">
-
+  <div class="p-4 min-h-screen max-w-screen-lg mx-auto">
     <div v-if="loading" class="text-center">Loading...</div>
     <div v-else-if="pokemon" class="pokemon-detail">
-      <div class="flex-col">
+      <div class="flex flex-col items-center w-full">
         <NuxtLink to="/pokedex" class="retro-button mb-8 inline-block">
           Back to List
         </NuxtLink>
+
         <div class="scanline"></div> <!-- Trait animé -->
         <h1 class="text-2xl mb-4 text-center text-shadow">{{ pokemonNameFR }}</h1>
 
-        <div class="text-center mb-8">
-          <img :src="pokemon.sprites.front_default" :alt="image" class="mx-auto -my-24"
-            style="width: 512px; height: 512px; image-rendering: pixelated;">
+        <div class="text-center mb-8 w-full flex justify-center">
+          <img :src="pokemon.sprites.front_default" :alt="image" class="mx-auto w-full max-w-lg"
+            style="image-rendering: pixelated;">
         </div>
 
-        <div class="m-auto w-5/6">
-          <p>
-            <span v-if="text" class="text-white text-xl text-shadow">{{ text }}</span>
+        <div class="m-auto w-full md:w-5/6">
+          <p v-if="text" class="text-white text-xl text-shadow text-center">
+            {{ text }}
           </p>
         </div>
-        <div class="m-auto w-5/6 mt-8">
+        <div class="m-auto w-full md:w-5/6 mt-8">
           <h3 class="text-xl text-shadow mb-4">Source: </h3>
           <iframe v-if="url" :src="url" class="w-full" style="height: 800px; border: none;">
           </iframe>
@@ -53,12 +53,10 @@ const formattedStats = computed(() => {
 });
 
 const getPokemonUrl = (pokemonName) => {
-  console.log("pokemonName", pokemonName);
   for (const issue in urlsPokemons.pokemon_issues) {
     if (urlsPokemons.pokemon_issues[issue].pokemons.includes(pokemonName)) {
       text = urlsPokemons.pokemon_issues[issue].text;
       url = urlsPokemons.pokemon_issues[issue].url;
-      console.log("text", text);
       return { text, url };
     }
   }
@@ -68,7 +66,6 @@ const getPokemonUrl = (pokemonName) => {
 let issue: any = null;
 let text: string | null = null;
 let url: string | null = null;
-
 let pokemonNameFR: string | null = null;
 
 async function getPokemonnNameFR(pokemonName: string) {
@@ -82,8 +79,12 @@ onMounted(async () => {
   pokemon.value = await getPokemonByName(name);
 
   issue = getPokemonUrl(name.charAt(0).toUpperCase() + name.slice(1));
+  if (issue) {
+    text = issue.text;
+    url = issue.url;
+  }
+
   pokemonNameFR = await getPokemonnNameFR(name);
-  console.log("pokemonNameFR", pokemonNameFR);
   loading.value = false;
 });
 </script>
@@ -117,26 +118,23 @@ onMounted(async () => {
 }
 
 .pokemon-detail {
-  display: flex;
-  flex-direction: row;
-  min-height: 100vh;
-  /* Remplacement du height par min-height */
   font-family: 'Press Start 2P', cursive;
   position: relative;
-  overflow-y: auto;
-  /* Ajout du défilement vertical */
   overflow-x: hidden;
   outline: none;
   background-color: #f0c838;
-  display: flex;
-  align-items: flex-start;
-  padding: 20px;
   z-index: 1;
   background: repeating-linear-gradient(180deg,
       #f0c838,
       #f0c838 2px,
       transparent 2px,
       transparent 3px);
+
+  /* Par défaut en colonne, centrée */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
 }
 
 .text-shadow {
