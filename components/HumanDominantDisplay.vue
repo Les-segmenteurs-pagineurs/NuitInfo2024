962c5f318420740bcceb5cfe5d94e04073885ef8
Mathieu ACKERMANN <116433624+Floupper@users.noreply.github.com>
2024-12-06 02:29:26 +0100

    feat: add HumanDominantScreen

--- components/HumanDominantDisplay.vue
+++ components/HumanDominantDisplay.vue
@@ -1,12 +1,21 @@
+<script setup lang="ts">
+// import "~/assets/css/game.scss";
+import "~/assets/css/bootstrapWish.scss";
+</script>
+
 <template>
-  
   <div class="main">
     <div class="left-side">
-      
       <div class="corner-pic">
+        <img
+          src="../assets/img/backgroundOceanScreen.svg"
+          alt="Background Ocean"
+          class="background-ocean"
+        />
         <slot />
       </div>
-      <div>
+      <div class="stats">
+        <h2>Explication</h2>
         <ul>
           <li>stat</li>
           <li>stat</li>
@@ -14,19 +23,104 @@
         </ul>
       </div>
     </div>
-    <div class="w-100 h-100 right-side">
-      content
+    <div class="h-100 right-side">
+      <img
+        src="../assets/img/backgroundHuman.svg"
+        alt="Background Ocean"
+        class="background-human"
+      />
+      <div class="human-icons">
+        <img src="../assets/img/hearth.svg" alt="Hearth Icon" class="hearth" />
+        <img src="../assets/img/kidneys.svg" alt="Kidney Icon" class="kidney" />
+        <img src="../assets/img/liver.svg" alt="liver Icon" class="liver" />
+        <img src="../assets/img/lungs.svg" alt="lungs Icon" class="lungs" />
+      </div>
     </div>
   </div>
-  
-
-
-
 </template>
 
+<style lang="css" scoped>
+.stats {
+  margin-top: 0.5rem;
+}
 
-<script setup lang="ts">
-import '~/assets/css/game.scss'
-import '~/assets/css/bootstrapWish.scss'
+.background-ocean {
+  position: absolute;
+  bottom: 0;
+  padding: 1rem;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 2.5rem;
+  background-color: white;
+}
 
-</script>
+.background-human {
+  bottom: 0;
+  padding: 1rem;
+  height: 100%;
+  object-fit: contain;
+}
+
+.main {
+  display: flex;
+  height: 100%;
+  position: relative;
+}
+
+.left-side {
+  position: absolute;
+  width: 23vw;
+  height: 100%;
+  display: flex;
+  z-index: 2;
+  flex-direction: column;
+}
+
+.corner-pic {
+  position: relative;
+  height: 35vh;
+  width: 23vw;
+  border-bottom-right-radius: 1rem;
+}
+
+.sw-btn {
+  position: absolute;
+  left: 21.5vw;
+  top: 25vh;
+}
+
+.right-side {
+  width: 80%;
+  left: 20%;
+  position: relative;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.human-icons {
+  position: absolute;
+}
+
+.hearth {
+  position: absolute;
+  top: -10vh;
+  left: 5vw;
+  width: 10vw;
+}
+
+.kidney {
+  position: absolute;
+  top: 30vh;
+  left: 8vw;
+  width: 10vw;
+}
+
+.liver {
+  position: absolute;
+  top: 30vh;
+  left: -7vw;
+  width: 10vw;
+}
+</style>
--- pages/game.vue
+++ pages/game.vue
@@ -1,38 +1,50 @@
-
 <template>
+  <div v-if="humanDisplay" class="h-100 w-100">
+    <HumanDominantDisplay>
+      <div class="h-100 w-100">
+        <UButton
+          square
+          class="fit sw-btn"
+          color="gray"
+          variant="solid"
+          icon="i-heroicons-arrows-right-left"
+          @click="humanDisplay = !humanDisplay"
+        />
+      </div>
+    </HumanDominantDisplay>
+  </div>
 
-    <div v-if="humanDisplay" class="h-100 w-100">
-        <HumanDominantDisplay>
-            <div class="h-100 w-100">
-                <UButton square class="fit sw-btn" color="gray" variant="solid" icon="i-heroicons-arrows-right-left" @click="humanDisplay = !humanDisplay"/>
-            </div>
-        </HumanDominantDisplay>
-    </div>
-
-    <div v-else class="h-100 w-100"> 
-        <OceanDominantDisplay>
-            <UButton square class="fit sw-btn" color="gray" variant="solid" icon="i-heroicons-arrows-right-left" @click="humanDisplay = !humanDisplay"/>
-        </OceanDominantDisplay>
-    </div>
-
-
-
+  <div v-else class="h-100 w-100">
+    <OceanDominantDisplay>
+      <UButton
+        square
+        class="fit sw-btn"
+        color="gray"
+        variant="solid"
+        icon="i-heroicons-arrows-right-left"
+        @click="humanDisplay = !humanDisplay"
+      />
+    </OceanDominantDisplay>
+  </div>
 </template>
 
-
 <script setup lang="ts">
-import "~/assets/css/bootstrapWish.scss"
-import {ref} from 'vue'
-import HumanDominantDisplay from '~/components/HumanDominantDisplay.vue';
+import "~/assets/css/bootstrapWish.scss";
+import { ref } from "vue";
+import HumanDominantDisplay from "~/components/HumanDominantDisplay.vue";
 
-let humanDisplay = ref(true)
-
+let humanDisplay = ref(true);
 </script>
 
 <style scoped>
-    .fit{
-        width: fit-content;
-        height: fit-content;
-    }
+.fit {
+  width: fit-content;
+  height: fit-content;
+}
 
+.sw-btn {
+  position: absolute;
+  left: 21.5vw;
+  top: 25vh;
+}
 </style>
