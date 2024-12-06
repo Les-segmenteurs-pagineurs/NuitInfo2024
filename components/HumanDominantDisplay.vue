ef2ffa2628b7f0b3167a6f93fdd3f818c6c2fef3
= <=>
2024-12-06 02:00:22 +0100

    updated prisma schema

--- components/HumanDominantDisplay.vue
+++ components/HumanDominantDisplay.vue
@@ -1,13 +1,7 @@
 <template>
-    
-
+  human dominant
   <UButton v-for="organ in props.Organs" class="fit" color="gray" variant="solid" :label="organ.name" @click="myOpenHumanModal(organ)"/>
-  
-    <!-- <UButton class="fit" color="gray" variant="solid" label="Heart" @click="myOpenHeartModal"/>
-    <UButton class="fit" color="gray" variant="solid" label="Liver" @click="myOpenLiverModal"/>
-    <UButton class="fit" color="gray" variant="solid" label="Lungs" @click="myOpenLungsModal"/>
-    <UButton class="fit" color="gray" variant="solid" label="Reins" @click="myOpenReinsModal"/> -->
-
+  <UButton v-for="organ in props.Organs" class="fit" color="gray" variant="solid" :label="organ.name" @click="myOpenHumanModal(organ)"/>
   
 </template>
 
@@ -30,7 +24,6 @@ function myOpenHumanModal(organ : any){
     props.OpenHumanModal(organ)
   }
 }
-
 
 
 </script>
--- components/OceanDominantDisplay.vue
+++ components/OceanDominantDisplay.vue
@@ -1,8 +1,28 @@
 <template>
-    ocean dominant
-    <slot />
-</template>
-
-
-<script setup lang="ts">
-</script>
+    sea dominant
+    <UButton v-for="organ in props.Organs" class="fit" color="gray" variant="solid" :label="organ.name" @click="myOpenSeaModal(organ)"/>
+  </template>
+  
+  
+  <script setup lang="ts">
+  
+  import '~/assets/css/game.scss'
+  import '~/assets/css/bootstrapWish.scss'
+  import { defineProps } from 'vue';
+  
+  
+  const props = defineProps({
+    Organs : Array<any>,
+      OpenSeaModal: Function,
+      
+    })
+  
+  function myOpenSeaModal(organ : any){
+    if (props.OpenSeaModal){
+      props.OpenSeaModal(organ)
+    }
+  }
+  
+  
+  </script>
+  
